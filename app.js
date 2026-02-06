1b88f9e0dadd095f4990ecdb28179d0296b2aa06
Pietro Scurti <pietroscurti9@gmail.com>
2026-02-06 16:35:37 +0100

    prime prove

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const posts = require('./posts');
+const app = express()
+const port = 3000
+
+const allPosts = posts
+
+app.use(express.static('posts'));
+
+
+
+
+app.get('/posts', function (req, res) {
+    res.send('Lista dei posts');
+});
+// show
+app.get('/posts/:id', function (req, res) {
+    res.send('Dettagli del post ' + req.params.id);
+});
+// store
+app.post('/posts', function (req, res) {
+    res.send('Creazione nuovo post');
+});
+// update
+app.put('/posts/:id', function (req, res) {
+    res.send('Modifica integrale del post' + req.params.id);
+});
+// modify
+app.patch('/posts/:id', function (req, res) {
+    res.send('Modifica parziale del post ' + req.params.id);
+});
+// destroy
+app.delete('/posts/:id', function (req, res) {
+    res.send('Eliminazione del post ' + req.params.id);
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
